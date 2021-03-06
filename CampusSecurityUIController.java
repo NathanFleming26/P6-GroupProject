6731385aca7b0d74de9d9e76d883e898076fa99f
CameronSavoury <31745632+CameronSavoury@users.noreply.github.com>
2021-03-06 14:27:28 +0000

    Updates to Controller and FXML Files
    Updated Controller class to make code more readable
    Removed code which other methods to be implemented will handle in separate classes
    Added data sanitisation class with regex methods for checking names and car registrations
    Added relevant warnings when incorrect car registration is entered
    
    Updated reference to controller in FXML file

--- CampusSecurityUIController.java
+++ CampusSecurityUIController.java
@@ -3,43 +3,75 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package campussecurityui;
+package application;
 
 import java.net.URL;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
-import static javafx.scene.paint.Color.color;
-import javafx.scene.text.TextAlignment;
 
-/**
- *
+/*
  * @author Cameron Savoury: cas00235
  */
 public class CampusSecurityUIController implements Initializable 
 {   
+    //<editor-fold defaultstate="collapsed" desc="Class Instance Variables">
+
+    //Class Instance Variables
     private Boolean barrierRaised;
     
+    RawDataSanitation RDS = new RawDataSanitation();
+    
+    Alert a;
+    
+//</editor-fold>
+    
     //<editor-fold defaultstate="collapsed" desc="Declare Elements">
-    @FXML
-    private Label lbl_barrierStatus;
-    @FXML
-    private Label lbl_warningMessage;
     
-    @FXML
-    private TextField txt_vehicleReg;
+    //<editor-fold defaultstate="collapsed" desc="Pane 1 Elements">
+    //Pane 1 Elements
+        @FXML
+        private Button P1btn_raiseBarrier;
+
+        @FXML
+        private Button P1btn_lowerBarrier;
+
+        @FXML
+        private Label P1lbl_barrierStatus;
+        
+        @FXML
+        private Label P1lbl_dynamicBarrierStatus;
+    //</editor-fold>
     
-    @FXML
-    private TableView<String> tbl_barrierLog = new TableView<>();
+    //<editor-fold defaultstate="collapsed" desc="Pane 2 Elements">
+        //Pane 2 Elements
+        @FXML
+        private Button P2btn_issueWarning;
+        
+        @FXML
+        private Label P2lbl_enterReg;
+        
+        @FXML
+        private TextField P2txt_vehicleReg;
+        //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Pane 3 Elements">
+        //Pane 3 Elements
+        @FXML
+        private Label P3lbl_barrierLog;
+        
+        @FXML
+        private TableView<String> P3tbl_barrierLog = new TableView<>();
+        //</editor-fold>
+    
 //</editor-fold>
     
     //<editor-fold defaultstate="collapsed" desc="Barrier Control Methods">
@@ -47,10 +79,10 @@ public class CampusSecurityUIController implements Initializable
     private boolean raiseBarrier (ActionEvent e)
     {
         System.out.println("Barrier Raised");
-        lbl_barrierStatus.setTextFill(Color.GREEN);
-        lbl_barrierStatus.setText("RAISED");
+        P1lbl_dynamicBarrierStatus.setTextFill(Color.GREEN);
+        P1lbl_dynamicBarrierStatus.setText("RAISED");
         barrierRaised = true;
-        System.out.println("\nBarrier Raised - " + barrierRaised.toString());
+        System.out.println("\nRaised - " + barrierRaised.toString());
         return barrierRaised;
     }//raiseBarrier
     
@@ -58,11 +90,11 @@ public class CampusSecurityUIController implements Initializable
     private boolean lowerBarrier (ActionEvent e)
     {
         System.out.println("Barrier Lowered");
-        lbl_barrierStatus.setTextFill(Color.RED);
-        lbl_barrierStatus.setText("LOWERED");
+        P1lbl_dynamicBarrierStatus.setTextFill(Color.RED);
+        P1lbl_dynamicBarrierStatus.setText("LOWERED");
         barrierRaised = false;
         System.out.println("\nRaised - " + barrierRaised.toString());
-        return barrierLowered;
+        return barrierRaised;
     }//lowerBarrier
 //</editor-fold>
     
@@ -71,15 +103,30 @@ public class CampusSecurityUIController implements Initializable
     private String issueWarning()
     {
         //get registration from textField
-        String vehicleReg = txt_vehicleReg.getText();
-        
-        System.out.println("\nVehicle Reg is: " + vehicleReg);
-        
-        lbl_warningMessage.setTextFill(Color.GREEN);
-        lbl_warningMessage.setTextAlignment(TextAlignment.CENTER);
-        lbl_warningMessage.setText("WARNING ISSUED");
-        
+        String vehicleReg = P2txt_vehicleReg.getText();
+
+        if (!RDS.isValidRegistration(vehicleReg)) 
+        {
+            // Pop error
+            a.setAlertType(AlertType.ERROR);
+            a.setHeaderText("Invalid UK Car Registration");
+            a.setTitle("Error with car registration field");
+            a.setContentText("Please enter a vaild UK car registration!");
+            a.show();
+            
+            P2txt_vehicleReg.clear();
+            return null;
+        }
+        else
+        {
+            a.setAlertType(AlertType.INFORMATION);
+            a.setHeaderText("Warning Confirmed");
+            a.setTitle("Warning Confirmation");
+            a.setContentText("Warning Issued for: " + vehicleReg);
+            a.show(); 
+            P2txt_vehicleReg.clear();
         return vehicleReg;
+        }
     }
     //</editor-fold>
     
@@ -87,34 +134,18 @@ public class CampusSecurityUIController implements Initializable
     @FXML
     private void logChange()
     {
-        String curTime;
-        String curDate;
-        
-        //Retrieve System Date and Time
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
-        LocalDateTime now = LocalDateTime.now();
-        
-        //Assign to Variables
-        curDate = dateFormat.format(now);
-        curTime = timeFormat.format(now);
-        
-        ObservableList<String> list = FXCollections.observableArrayList();
-        tbl_barrierLog.setItems(list);
-        list.add(curTime);
-        list.add(curDate);
+        //TODO - Add code from system status class
     }
 //</editor-fold>
     
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     {
-        //Set All Labels to their default values
-        lbl_barrierStatus.setTextFill(Color.BLACK);
-        lbl_barrierStatus.setText(""); 
+        a = new Alert(AlertType.NONE);
         
-        lbl_warningMessage.setTextFill(Color.BLACK);
-        lbl_warningMessage.setText("");
+        //Set All Labels to their default values
+        P1lbl_dynamicBarrierStatus.setTextFill(Color.BLACK);
+        P1lbl_dynamicBarrierStatus.setText("");
     }//Initialize 
     
 }//End of Class
