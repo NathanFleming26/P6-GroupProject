b85f814096d4e26bb129d2253f2c22219d991eef
brucespe <77619592+brucespe@users.noreply.github.com>
2021-04-01 11:08:59 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -15,6 +15,7 @@ public class Main {
      * They are presumed to put themselves on display, and register as Observers as necessary.
      */
     public static void main(String[] args) {
+    	//Instantiate classes required for later use
         System_status systemStatus = new System_status();
         Vehicle_list vehicleList = new Vehicle_list();
         Permit_list permitList = new Permit_list();
@@ -22,6 +23,7 @@ public class Main {
         Timer timer = new Timer(systemStatus, permitList);     // Frame, boundary class
 
         Barrier barrier1 = new Barrier(systemStatus, vehicleList);  // Frame, boundary class
+        Barrier barrier2 =  new Barrier(systemStatus, vehicleList);
           // Repeat for as many barriers as required
 
         Campus_security campusSecurity1 = new Campus_security(systemStatus, vehicleList);  // Frame, boundary class
--- Vehicle_list.java
+++ Vehicle_list.java
@@ -12,97 +12,107 @@ import java.util.Hashtable;
  * There will only be one instance of this class.
  */
 public class Vehicle_list {
-    /** The Vehicle list maintains a collection of the known Vehicle_infos associated with all permits.
-     * This association must be implemented by an attribute holding a collection data structure
-     * (for example: array, hash table - the latter is recommended).
-     *
-     * Note that no two Vehicle_infos may have the same registration number (this information is
-     * not represented diagrammatically) - this is to guarantee consistency with the constraint
-     * that no vehicle is associated with more than one permit.
-     *
-     * Note that, given a registration number, the Vehicle_list can look up the appropriate Vehicle_info
-     * instance, and via that it can obtain the vehicle's permit information.
-     * @associates Vehicle_info
-     * @label Contains
-     * @clientCardinality 1
-     * @supplierCardinality 0..*
-     * @directed*/
-    private Hashtable<String, Vehicle_info> lnkVehicle;
-    private Permit_list P;
-    
-    public Vehicle_list() {
-//        //Initialise and populate hashtable
-    	lnkVehicle = new Hashtable<String, Vehicle_info>();
-//
-        Vehicle_info v1 = new Vehicle_info("BB58 UCE", "make1", "model1", "colour1", "Bruce");     //need to add other attributes too
-        lnkVehicle.put("BB58 UCE", v1);
-//
-//        Vehicle_info v2 = new Vehicle_info("SW65 NTJ", "make2", "model2", "colour2", 2);
-//        lnkVehicle.put(v2.getReg(), v2);
-//
-//        Vehicle_info v3 = new Vehicle_info("AB23 SKG", "make3", "model3", "colour3", 3);
-//        lnkVehicle.put(v3.getReg(), v3);
-//        
-//        Vehicle_info v4 = new Vehicle_info("DI24 SKG", "make4", "model4", "colour4", 4);
-//        lnkVehicle.put(v4.getReg(), v4);
+	/** The Vehicle list maintains a collection of the known Vehicle_infos associated with all permits.
+	 * This association must be implemented by an attribute holding a collection data structure
+	 * (for example: array, hash table - the latter is recommended).
+	 *
+	 * Note that no two Vehicle_infos may have the same registration number (this information is
+	 * not represented diagrammatically) - this is to guarantee consistency with the constraint
+	 * that no vehicle is associated with more than one permit.
+	 *
+	 * Note that, given a registration number, the Vehicle_list can look up the appropriate Vehicle_info
+	 * instance, and via that it can obtain the vehicle's permit information.
+	 * @associates Vehicle_info
+	 * @label Contains
+	 * @clientCardinality 1
+	 * @supplierCardinality 0..*
+	 * @directed*/
+	//Instance Variables
+	private Hashtable<String, Vehicle_info> lnkVehicle;
+	private Permit_list P;
 
-    }
-    public boolean addVehicle(String reg, String make, String model, String colour, String name) {
-      if (lnkVehicle.containsKey(reg)) { 
-    	  return false;
-      } else {
-      	Vehicle_info v = new Vehicle_info(reg, make, model, colour, name);
-      	lnkVehicle.put(v.getReg(), v);
-      	return true;
-      }
+	//Empty Constructor
+	public Vehicle_list() {
+		//Initialise hashtable
+		lnkVehicle = new Hashtable<String, Vehicle_info>();
 
-    }
-    public Vehicle_info getVehicle(String reg) {
-    	Vehicle_info v;
-        if (lnkVehicle.containsKey(reg)) { 
-        	v = (Vehicle_info) lnkVehicle.get(reg);
-        	return v;
-        }else {
-        	return v=null;
-        }
-    }
-    public boolean setWarning(String reg) {
-    	Vehicle_info v;
-        if (lnkVehicle.containsKey(reg)) { 
-        	v = lnkVehicle.get(reg);
-        	v.setWarning();
-        	return true;
-        }else {
-        	return false;
-        }
-        }
-        public int getWarning(String reg) {
-        	Vehicle_info v;
-            if (lnkVehicle.containsKey(reg)) { 
-            	v = lnkVehicle.get(reg);
-            	return v.getWarning();
-            	//v.setWarning();
-            }
-            return 0;
-            	
-    }
-    public boolean deleteWarning(String reg) {
-    	Vehicle_info v;
-        if (lnkVehicle.containsKey(reg)) { 
-        	v = (Vehicle_info) lnkVehicle.get(reg);
-        	v.deleteWarning();
-        	return true;
-        }else {
-        	return false;
-        }
-    }
-    
-    
-//    public void addVehicle(String regNo) {          //Add vehicle to has table, need some way to return if vehicle was successfully added
-//        if (lnkVehicles.containsKey(regNo)) {       //This hashtable already contains a vehicle with this reg, should add return error message
-//        } else {
-//            Vehicle_info v = new Vehicle_info("regNo");
-//            lnkVehicles.put(regNo, v);              //Add vehicle to has table
-//        }
-//    }
+	}
+	public boolean addVehicle(String reg, String make, String model, String colour, String name) {//Adds a vehicle to the hashtable
+		if (lnkVehicle.containsKey(reg)) { //if vehicle with reg already exists, return false
+			return false;
+		} else {
+			Vehicle_info v = new Vehicle_info(reg, make, model, colour, name);//Initialise a new Vehicle and add to hastable
+			lnkVehicle.put(v.getReg(), v);
+			return true;
+		}
+
+	}
+	public Vehicle_info getVehicle(String reg) {//retunrs the vehicle from the hashtable usinf the reg as the key
+		Vehicle_info v;
+		if (lnkVehicle.containsKey(reg)) { 
+			v = (Vehicle_info) lnkVehicle.get(reg);
+			return v;
+		}else {
+			return v=null;
+		}
+	}
+
+	public boolean setWarning(String reg) {//Adds a warning to the vehicle with the entered reg
+		Vehicle_info v;
+		if (lnkVehicle.containsKey(reg)) { 
+			v = lnkVehicle.get(reg);
+			if (v.getWarning()<3){
+				v.setWarning();
+				return true;
+			}
+			if(v.getWarning()==3) {
+				v.setSuspended(true);
+			}
+		}
+		return false;
+	}
+	public int getWarning(String reg) {//Returns the number of warnings a vehicle has
+		Vehicle_info v;
+		if (lnkVehicle.containsKey(reg)) { 
+			v = lnkVehicle.get(reg);
+			return v.getWarning();
+			//v.setWarning();
+		}
+		return 0;
+
+	}
+	public boolean getSuspended(String reg) {
+		Vehicle_info v;
+		if (lnkVehicle.containsKey(reg)) { 
+			v = lnkVehicle.get(reg);
+			if (v.getSuspended()==true) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean deleteWarning(String reg) {//Reduces the value of warnings for the vehicle by 1
+		Vehicle_info v;
+		if (lnkVehicle.containsKey(reg)) { 
+			v = (Vehicle_info) lnkVehicle.get(reg);
+			if (v.getWarning()==0) {
+				return false;
+			}else {
+				v.deleteWarning();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean vehicleExists(String reg)//Returns true if the vehicle is already in the hashtable
+	{
+		if (lnkVehicle.containsKey(reg))
+		{
+			return true;
+		}else
+		{
+			return false;
+		}
+	}
 }
